9ff3e26103ecea1cc1c6dbedd8eb86170153f871
Chrisbry <c.bryantcf@gmail.com>
2022-09-29 11:48:53 -0400

    Java File Config

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("org.example")
+@ComponentScan(basePackages = "org.example")
 public class AppConfig {
 
 //    @Bean
